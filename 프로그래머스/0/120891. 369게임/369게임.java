1a7ac9c82faaf3f65e458b2d0c1fc763854756d0
이동현 <133379681+ldxhyeon@users.noreply.github.com>
2025-01-27 14:32:48 +0900

    [level 0] Title: 369게임, Time: 0.15 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int order) {
+        
+        int answer = 0;
+        
+        String[] arr = String.valueOf(order).split("");
+        
+        for(int i = 0; i < arr.length; i++) {
+            
+           if(arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
