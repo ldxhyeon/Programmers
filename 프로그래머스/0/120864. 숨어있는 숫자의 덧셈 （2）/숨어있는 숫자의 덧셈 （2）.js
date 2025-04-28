0640668daca734f5915e6aaef072df24eff2f35b
이동현 <133379681+ldxhyeon@users.noreply.github.com>
2025-04-28 11:02:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,10 +1,12 @@
 function solution(my_string) {
     const regex = /[^0-9]/g;
+    // 정규식으로 숫자 배열 만들기
+    // Number타입으로 변환하여 배열 만들기
     const result = my_string.split(regex).map(Number);
     let sum = 0;
     
     for(let i = 0; i < result.length; i++) {
-        if(!isNaN(result[i])) {
+        if(result[i] != 0) {
             sum += result[i];
         }
     }
